000de99dd301b1eeb62f97a307d81cdad96b97a0
Shurui Liu <shurui91@gmail.com>
2016-09-13 11:24:03 -0700

    be able to get XML file content now

--- hw6_stock/stock.php
+++ hw6_stock/stock.php
@@ -6,49 +6,74 @@
 		<title>Stock Search with Market Data APIs</title>
 		<style>
 		#mainbox, #output_box {
-			width: 400px;
+			width: 500px;
 			margin: 0 auto;
-			border: 2px solid black;
-			padding: 10px;
+			border: 1px solid;
 			line-height: 20pt;
-		}
-		h1 {
 			text-align: center;
 		}
-		#website {
-			text-align: center;
+		h1 {text-align: center;}
+		#website, #button_group {text-align: center;}
+		table, td, th {
+			border: 1px solid;
 		}
-		#button_group {
-			text-align: center;
-		}
+		table {
+			width: 500px;
+			border-collapse: collapse;
+		};
 	</style>
 	</head>
 	<body>
 		<h1>Stock Search</h1>
 		<div id="mainbox">
-		<form id="myForm" onsubmit="return validateForm()" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-			Company Name or Symbol: <input type="text" name="stock_name"><br/>
+			<form id="myForm" onsubmit="return validateForm()" method="post">
+				Company Name or Symbol: <input type="text" name="stock_name"><br/>
+				<div id="button_group">
+					<button type="submit" value="Submit">Search</input>
+					<button type="reset" onclick="formReset();divRemove();" value="Reset">Clear</button>
+					<br/>
+				</div>
+				<div id="website">
+					<a href='http://www.markit.com/product/markit-on-demand'>Powered by Market on Demand</a>
+				</div>
+			</form>
+		</div>
+		<div>
 			<?php
-				$stock = '';
-				$url = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/xml?input=';
-				if ($_SERVER["REQUEST_METHOD"] == "POST") {
-					$stock .= $_POST['stock_name'];
-				}
-				$url = $url . $stock;
-				$xmlFile = simplexml_load_string($url);
-				
-				// start to read content from xml file
-				
+			$stock = '';
+			$url = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/xml?input=';
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				$stock .= $_POST['stock_name'];
+			}
+			$stockUrl = $url . $stock;
+			// echo $stockUrl;
+			// start to read content from xml file
+			$urlContent = file_get_contents($stockUrl);
+			$xmlFile = simplexml_load_string($urlContent);
+			if (strlen($xmlFile) > 0) {
+				echo "<div id=\"output_box\">
+						<table>
+							<tr>
+								<th>Name</th>
+								<th>Symbol</th>
+								<th>Exchange</th>
+								<th>Details</th>
+							</tr>";
+			}
 			?>
-			<div id="button_group">
-				<button type="submit" value="Submit">Search</input>
-				<button type="reset" onclick="formReset();divRemove();" value="Reset">Clear</button>
-				<br/>
+			
+					<?php
+						foreach ($xmlFile -> LookupResult as $xml) {
+							echo "<tr>";
+							echo "<td>" . $xml -> Name . "</td>";
+							echo "<td>" . $xml -> Symbol . "</td>";
+							echo "<td>" . $xml -> Exchange . "</td>";
+							echo "<td><a href=''>More Info</a></td>";
+							echo "</tr>";
+						}
+					?>
+				</table>
 			</div>
-			<div id="website">
-				<a href='http://www.markit.com/product/markit-on-demand'>Powered by Market on Demand</a>
-			</div>
-		</form>
 		</div>
 		<script>
 			function formReset() {
